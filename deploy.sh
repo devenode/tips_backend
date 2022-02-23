c8e8276177b3c270005f160d2d6659e991c1ef2c
Nastromo <nastromo@solaris-2.local>
2022-02-23 17:55:28 +0200

    Deploy setup

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+rm -rf logs/* logs/.*
 rm -rf prod/* prod/.*
 touch prod/.env
 cp -v prod.env prod/.env
 
 cp -R api prod
+cp -R logs prod
 cp -R middleware prod
+cp -R models prod
 cp -R utils prod
 
 cp app.js prod
+cp db.js prod
 cp package.json prod
 
 cd prod
 npm install
-tar czf app.tar.gz api/ middleware/ node_modules/ utils/ app.js package.json .env
+tar czf app.tar.gz api/ logs/ middleware/ models/ node_modules/ utils/ app.js db.js package.json .env
 
 scp app.tar.gz nastromo@165.22.18.107:~/tips
 rm app.tar.gz
